b5eb7554c81243a17713c5444d61e66b5067ba30
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-08-11 06:24:46 -0300

    add example home

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './page/home.component';
 import { HomeRoutingModule } from './home.routing';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [HomeRoutingModule],
+  imports: [HomeRoutingModule, MatGridListModule],
 })
 export class HomeModule {}
