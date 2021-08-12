e602fa05820b549683ed0e3afc0c31ebf14f7e08
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-08-12 18:32:34 -0300

    add service to component user

--- src/app/modules/user/page/users.component.ts
+++ src/app/modules/user/page/users.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { User } from '../data/user.model';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css'],
 })
-export class UsersComponent {}
+export class UsersComponent {
+  allUser: User[] = this.userService.getMockAllUsers();
+
+  constructor(private userService: UserService) {}
+}
