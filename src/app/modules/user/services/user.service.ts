3ccdc53dcc6487560e614fe6ff879ce8cfffc1ae
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-08-12 18:29:59 -0300

    refactor service user

--- src/app/modules/user/services/user.service.ts
+++ src/app/modules/user/services/user.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { User } from '../data/user.model';
 import { MOCKUSER } from '../data/mockUser';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  getMockUsers() {
-    return MOCKUSER;
+  private mockAllUser: User[] = MOCKUSER;
+
+  getMockAllUsers() {
+    return this.mockAllUser;
   }
 }
