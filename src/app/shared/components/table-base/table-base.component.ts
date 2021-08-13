d181e245eca5df54bf4ae5b1c7be31e4423945be
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-08-13 05:51:56 -0300

    add filter

--- src/app/shared/components/table-base/table-base.component.ts
+++ src/app/shared/components/table-base/table-base.component.ts
@@ -1,11 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-table-base',
   styleUrls: ['table-base.component.css'],
   templateUrl: 'table-base.component.html',
 })
-export class TableBaseComponent {
+export class TableBaseComponent implements OnInit {
   @Input() displayedColumns: string[] = [];
   @Input() dataSource: any = [];
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource(this.dataSource);
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { TableBaseComponent } from './components/table-base/table-base.component';
 import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [TableBaseComponent],
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, MatFormFieldModule, MatInputModule],
   exports: [TableBaseComponent],
 })
 export class SharedModule {}
