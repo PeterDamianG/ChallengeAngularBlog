9f371c048e6bcd9a51cf6e61698ebd0a54bf0ad5
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-08-13 04:39:51 -0300

    refactor base tablet

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableBaseComponent } from './components/table-base/table-base.component';
 import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [TableBaseComponent],
-  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule],
+  imports: [CommonModule, MatTableModule],
   exports: [TableBaseComponent],
 })
 export class SharedModule {}
