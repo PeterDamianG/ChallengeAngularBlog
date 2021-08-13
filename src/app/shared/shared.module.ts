953e090c54cc2a315448d4018b30db4b5c5b258d
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-08-13 04:03:36 -0300

    add column generate based in propiety binding

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TableBaseComponent } from './components/table-base/table-base.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
@@ -6,7 +7,7 @@ import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [TableBaseComponent],
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule],
   exports: [TableBaseComponent],
 })
 export class SharedModule {}
