fd95fb81ce378273581bf0c60184a5a81221101f
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-08-13 05:59:35 -0300

    add pagination

--- src/app/shared/components/table-base/table-base.component.ts
+++ src/app/shared/components/table-base/table-base.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-table-base',
@@ -9,13 +11,22 @@ import { MatTableDataSource } from '@angular/material/table';
 export class TableBaseComponent implements OnInit {
   @Input() displayedColumns: string[] = [];
   @Input() dataSource: any = [];
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource(this.dataSource);
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
-  ngOnInit(): void {
-    this.dataSource = new MatTableDataSource(this.dataSource);
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatInputModule } from '@angular/material/input';
 import { TableBaseComponent } from './components/table-base/table-base.component';
 import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [TableBaseComponent],
-  imports: [CommonModule, MatTableModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+  ],
   exports: [TableBaseComponent],
 })
 export class SharedModule {}
