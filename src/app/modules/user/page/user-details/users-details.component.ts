80e6775d9c47299edb2d8d4648a069f924947e3d
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-08-13 09:11:40 -0300

    create a user details component

---
+++ src/app/modules/user/page/user-details/users-details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-details',
+  templateUrl: './users-details.component.html',
+  styleUrls: ['./users-details.component.css'],
+})
+export class UsersDetailsComponent {}
