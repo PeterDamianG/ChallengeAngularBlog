bc7dc1482b4caba934ad62da1aed877294e9e9ef
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-08-15 18:52:01 -0300

    add responsively and animation to home component

--- src/app/core/page/home/home.component.ts
+++ src/app/core/page/home/home.component.ts
@@ -1,8 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnDestroy {
+  columns: string = '2';
+  destroyed = new Subject<void>();
+  displayNameMap = new Map([
+    [Breakpoints.XSmall, '1'],
+    [Breakpoints.Small, '1'],
+    [Breakpoints.Medium, '2'],
+    [Breakpoints.Large, '2'],
+    [Breakpoints.XLarge, '2'],
+  ]);
+
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver
+      .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge,
+      ])
+      .pipe(takeUntil(this.destroyed))
+      .subscribe((result) => {
+        for (const query of Object.keys(result.breakpoints)) {
+          if (result.breakpoints[query]) {
+            this.columns = this.displayNameMap.get(query) ?? 'Unknown';
+          }
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
+}
