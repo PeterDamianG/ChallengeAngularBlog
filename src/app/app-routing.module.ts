c80d872cb2d1fcee2b8564c2b56b3a45cdba1717
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-08-15 07:18:08 -0300

    add animation to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,28 +12,37 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+    data: { animation: 'base' },
   },
   {
     path: 'users',
     loadChildren: () =>
       import('./modules/user/user.module').then((m) => m.UserModule),
+    data: { animation: 'bases' },
   },
   {
     path: 'posts',
     loadChildren: () =>
       import('./modules/post/post.module').then((m) => m.PostModule),
+    data: { animation: 'bases' },
   },
   {
     path: 'albums',
     loadChildren: () =>
       import('./modules/album/album.module').then((m) => m.AlbumModule),
+    data: { animation: 'bases' },
   },
   {
     path: 'todos',
     loadChildren: () =>
       import('./modules/todo/todo.module').then((m) => m.TodoModule),
+    data: { animation: 'bases' },
   },
-  { path: '404', component: NotFoundPageComponent },
+  {
+    path: '404',
+    component: NotFoundPageComponent,
+    data: { animation: 'bases' },
+  },
   { path: '**', redirectTo: '/404', pathMatch: 'full' },
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { slideInAnimation } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  animations: [slideInAnimation],
 })
 export class AppComponent {
   title = 'blog-angular';
+  getAnimationData(outlet: RouterOutlet) {
+    return (
+      outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation
+    );
+  }
 }
